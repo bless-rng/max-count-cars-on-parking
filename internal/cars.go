d493d6440ca2c4dd1249a49f145ae727da276827
Maks Vesnin <mvvesnin@stream.ry>
2021-07-10 01:03:30 +0300

    add new algo

--- internal/cars.go
+++ internal/cars.go
@@ -2,32 +2,34 @@ package parking
 
 import (
 	"sort"
+	"strconv"
 )
 
-func CalculateMaxCarsV1(parkingTickets *[]Ticket) int {
+func CalculateMaxCarsV1(parkingTickets []Ticket) int {
 	type tempState struct {
-		time    *string
+		time    string
 		isStart bool
 	}
 
-	totalTicketsCount := len(*parkingTickets)
+	totalTicketsCount := len(parkingTickets)
 
 	states := make([]tempState, totalTicketsCount*2, totalTicketsCount*2)
 
 	j := 0
-	for i := range *parkingTickets {
-		states[j] = tempState{&(*parkingTickets)[i].Start, true}
+
+	for i :=0; i <len(parkingTickets); i++ {
+		states[j] = tempState{parkingTickets[i].Start, true}
 		j++
-		states[j] = tempState{&(*parkingTickets)[i].End, false}
+		states[j] = tempState{parkingTickets[i].End, false}
 		j++
 	}
 	sort.Slice(states, func(i, j int) bool {
 		firstTime := states[i]
 		secondTime := states[j]
-		if *firstTime.time < *secondTime.time {
+		if firstTime.time < secondTime.time {
 			return true
 		}
-		if *firstTime.time == *secondTime.time {
+		if firstTime.time == secondTime.time {
 			return firstTime.isStart
 		}
 		return false
@@ -36,8 +38,8 @@ func CalculateMaxCarsV1(parkingTickets *[]Ticket) int {
 	maxCarsCount := 0
 	counter := 0
 	totalArrivalChecked := 0
-	for _, time := range states {
-		if time.isStart {
+	for _, state := range states {
+		if state.isStart {
 			counter += 1
 			totalArrivalChecked += 1
 			if maxCarsCount < counter {
@@ -51,4 +53,30 @@ func CalculateMaxCarsV1(parkingTickets *[]Ticket) int {
 		}
 	}
 	return maxCarsCount
+}
+
+
+func CalculateCarsByArray(parkingTickets []Ticket) int {
+	day := make([]int, 31*60*24)
+	start, end := 0 ,0
+	for i:=0; i<len(parkingTickets); i++ {
+		start, end = parseTicketTimes(parkingTickets[i])
+		for start <= end {
+			day[start]++
+			start++
+		}
+	}
+	sort.Ints(day)
+
+	return day[len(day) - 1]
+}
+
+func parseTicketTimes(ticket Ticket) (from int,to int) {
+	startHours, _ := strconv.Atoi(ticket.Start[0:2])
+	startMinutes, _ := strconv.Atoi(ticket.Start[3:])
+
+	endHours, _ := strconv.Atoi(ticket.End[0:2])
+	endMinutes, _ := strconv.Atoi(ticket.End[3:])
+
+	return startHours* 60 + startMinutes, endHours * 60 + endMinutes
 }
--- internal/cars_test.go
+++ internal/cars_test.go
@@ -7,9 +7,34 @@ import (
 func TestCalculateMaxCarsV1(t *testing.T) {
 	parkingTickets, expected := getTestData()
 	t.Log(len(parkingTickets))
-	result := CalculateMaxCarsV1(&parkingTickets)
+	result := CalculateMaxCarsV1(parkingTickets)
 	if result != expected {
 		t.Errorf("result is %d; expected %d", result, expected)
+	}
+}
+
+func TestCalculateCarsByArray(t *testing.T) {
+	parkingTickets, expected := getTestData()
+	t.Log(len(parkingTickets))
+	result := CalculateCarsByArray(parkingTickets)
+	if result != expected {
+		t.Errorf("result is %d; expected %d", result, expected)
+	}
+}
+
+func BenchmarkCalculateMaxCarsV1(b *testing.B) {
+	parkingTickets, _ := getTestData()
+	b.Log(len(parkingTickets))
+	for i := 0; i < b.N; i++ {
+		CalculateMaxCarsV1(parkingTickets)
+	}
+}
+
+func BenchmarkCalculateMaxCarsByArray(b *testing.B) {
+	parkingTickets, _ := getTestData()
+	b.Log(len(parkingTickets))
+	for i := 0; i < b.N; i++ {
+		CalculateCarsByArray(parkingTickets)
 	}
 }
 
@@ -26,7 +51,7 @@ func getTestData() (parkingTickets []Ticket, expected int) {
 		{"01:00","23:59"},
 		{"00:58","02:00"},
 	}, 5
-	repeatTimes := 10000
+	repeatTimes := 999999
 	var resultTickets []Ticket
 	for i := 0; i < repeatTimes; i++ {
 		resultTickets = append(resultTickets, tickets...)
